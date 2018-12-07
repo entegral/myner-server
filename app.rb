7463f79be43ff8a59122b71b5987ccceaccf470b
Rob <e2robby@gmail.com>
2018-12-06 22:44:37 -0800

    added newline split functionality to all server url routes

--- app.rb
+++ app.rb
@@ -13,27 +13,31 @@ end
 get '/start' do
   myner = Myner.new
   myner.start
-  @logs = myner.logs
+  logs = myner.logs
+  @logs = logs.split("\n")
   erb(:index)
 end
 
 get '/stop' do
   myner = Myner.new
   myner.stop
-  @logs = myner.logs
+  logs = myner.logs
+  @logs = logs.split("\n")
   erb(:index)
 end
 
 get '/setgpu' do
   myner = Myner.new
   myner.setgpu
-  @logs = myner.logs
+  logs = myner.logs
+  @logs = logs.split("\n")
   erb(:index)
 end
 
 get '/clear' do
   myner = Myner.new
   myner.clear
-  @logs = myner.logs
+  logs = myner.logs
+  @logs = logs.split("\n")
   erb(:index)
 end
